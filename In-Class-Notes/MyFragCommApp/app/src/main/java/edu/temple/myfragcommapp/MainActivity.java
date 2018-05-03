c37b76ab1fc510424f5b4be076ba919c06966d77
thatyanamorales <thtnmorales77@gmail.com>
2018-05-02 20:08:32 -0400

    Please work
    plz

--- In-Class-Notes/MyFragCommApp/app/src/main/java/edu/temple/myfragcommapp/MainActivity.java
+++ In-Class-Notes/MyFragCommApp/app/src/main/java/edu/temple/myfragcommapp/MainActivity.java
@@ -2,24 +2,38 @@ package edu.temple.myfragcommapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.support.v4.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity implements InitiateFragment.InitiateInterface{
+public class MainActivity extends AppCompatActivity{
 
-    RandomFragment randomFragment;
+    //RandomFragment randomFragment;
 
+    FragmentManager fm;
 
+    boolean twoPanes;
+    DisplayFragment dFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        randomFragment = new RandomFragment();
+        twoPanes = !(findViewById(R.id.container2) == null);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.randomContainer, randomFragment).commit();
+//        randomFragment = new RandomFragment();
+//
+//        getSupportFragmentManager().beginTransaction().replace(R.id.randomContainer, randomFragment).commit();
+
+        fm = getSupportFragmentManager();
+        InitiateFragment iFragment = new InitiateFragment();
+
+        fm.beginTransaction().replace(R.id.container1, iFragment).commit();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.initiatorContainer, new InitiateFragment()).commit();
+
+
 
 //        findViewById(R.id.newNumberButton).setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -29,9 +43,35 @@ public class MainActivity extends AppCompatActivity implements InitiateFragment.
 //        });
     }
 
-    @Override
-    public void buttonClicked(){
-        randomFragment.generateNewNumber();
+    public void buttonClicked(int i){
+        //randomFragment.generateNewNumber();
+
+//        DisplayFragment dFragment = new DisplayFragment();
+//
+//        Bundle bundle = new Bundle(); //key value pair object
+//        bundle.putInt("display_number", i);
+//
+//        dFragment.setArguments(bundle);
+
+//        DisplayFragment dFragment = DisplayFragment.newInstance(i);
+//
+//        fm.beginTransaction()
+//                .replace(R.id.container1, dFragment)
+//                .addToBackStack(null)
+//                .commit();
+
+        if(!twoPanes){
+            dFragment = DisplayFragment.newInstance(0);
+            fm.beginTransaction()
+                    .replace(R.id.container2, dFragment)
+                    .commit();
+        }
+
+        else{
+            dFragment.displayNumber(i);
+        }
+
+        //dFragment.displayNumber(i);
 
     }
 
